04ff00e8141131dd523d568816c5b2441a3db4bc
Iceberry <qddwork@outlook.com>
2022-07-12 09:56:34 +0800

    🔧enable swagger plugin

--- src/main/java/com/iceberry/starterDemo/config/SwaggerConfiguration.java
+++ src/main/java/com/iceberry/starterDemo/config/SwaggerConfiguration.java
@@ -1,17 +1,31 @@
 package com.iceberry.starterDemo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
 
 @Configuration
-
 public class SwaggerConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.Iceberry.starterDemo"))
+                .build();
+    }
+
+    Contact contact = new Contact("Iceberry", "Iceberry.com", "qddwork@outlook.com");
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("BookStore", "The book store application's api doc.", "1.0", "iceberry.com", contact, "MIT", "https://mit-license.org/", new ArrayList<>());
     }
 }
